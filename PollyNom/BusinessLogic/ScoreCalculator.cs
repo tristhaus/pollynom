fca60197975ed252ef216f5b9b0df4b62e273df2
tristhaus <tristhaus@yahoo.de>
2018-07-08 20:26:18 +0200

    Add business logic for the calculation of scores, including for several runs of dots

---
+++ PollyNom/BusinessLogic/ScoreCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PollyNom.BusinessLogic
+{
+    public static class ScoreCalculator
+    {
+        public static int CalculateScore(List <int> numbersOfHits)
+        {
+            return numbersOfHits.Where(x => (x > 0)).Select(x => ((int)Math.Pow(2, x) - 1)).Sum();
+        }
+    }
+}
---
+++ PollyNomTest/Scoring.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using PollyNom.BusinessLogic;
+
+namespace PollyNomTest
+{
+    [TestClass]
+    public class Scoring
+    {
+        [TestMethod]
+        public void SimpleCase()
+        {
+            // Arrange
+            List<int> list = new List<int>() { 3 };
+
+            // Act
+            int score = ScoreCalculator.CalculateScore(list);
+
+            // Assert
+            Assert.IsTrue(7 == score);
+        }
+
+        [TestMethod]
+        public void EmptyList()
+        {
+            // Arrange
+            List<int> list = new List<int>() { 0 };
+
+            // Act
+            int score = ScoreCalculator.CalculateScore(list);
+
+            // Assert
+            Assert.IsTrue(0 == score);
+        }
+
+        [TestMethod]
+        public void CombinedCase()
+        {
+            // Arrange
+            List<int> list = new List<int>() { 4, 0, 2 };
+
+            // Act
+            int score = ScoreCalculator.CalculateScore(list);
+
+            // Assert
+            Assert.IsTrue(15 + 3 == score);
+        }
+    }
+}

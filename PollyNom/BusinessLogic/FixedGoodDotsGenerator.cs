6bfb18037ea984d9676c92532160b5d954e8025a
tristhaus <tristhaus@yahoo.de>
2018-07-21 10:27:24 +0200

    Add a good dot generator that provides a const list of points for debugging purposes.

---
+++ PollyNom/BusinessLogic/FixedGoodDotsGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PollyNom.BusinessLogic.Dots;
+
+namespace PollyNom.BusinessLogic
+{
+    /// <summary>
+    /// Creates good dots on a grid in a random manner.
+    /// </summary>
+    public class FixedGoodDotsGenerator
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="FixedGoodDotsGenerator"/> class
+        /// that will generate the hard-coded dots.
+        /// </summary>
+        public FixedGoodDotsGenerator()
+        {
+        }
+
+        /// <summary>
+        /// Generates a list of good dots 
+        /// </summary>
+        /// <returns></returns>
+        public List<IDot> Generate()
+        {
+            List<IDot> list = new List<IDot>();
+            //list.Add(new Dots.GoodDot(0.0, 0.0));
+            //list.Add(new Dots.GoodDot(4.0, 0.0));
+            //list.Add(new Dots.GoodDot(1.0, 1.25));
+
+            list.Add(new Dots.GoodDot(-3.0, -3.0));
+            // list.Add(new Dots.GoodDot(-3.0, 3.0));
+            // list.Add(new Dots.GoodDot(3.0, -3.0));
+            
+            // list.Add(new Dots.GoodDot(1.0, 0.0));
+            // list.Add(new Dots.GoodDot(0.0, -1.0));
+
+            return list;
+        }
+    }
+}

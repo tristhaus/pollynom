e1e39dcbd3bd7db80b09a467f4cd874fb1972b47
tristhaus <tristhaus@yahoo.de>
2018-09-29 14:10:54 +0200

    Fix more SA issues

--- PollyFoundation/Properties/AssemblyInfo.cs
+++ PollyFoundation/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -21,21 +19,25 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// In order to begin building localizable applications, set
-// <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-// inside a <PropertyGroup>.  For example, if you are using US english
-// in your source files, set the <UICulture> to en-US.  Then uncomment
-// the NeutralResourceLanguage attribute below.  Update the "en-US" in
-// the line below to match the UICulture setting in the project file.
-// [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+/*
+ * In order to begin building localizable applications, set
+ * <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+ * inside a <PropertyGroup>.  For example, if you are using US english
+ * in your source files, set the <UICulture> to en-US.  Then uncomment
+ * the NeutralResourceLanguage attribute below.  Update the "en-US" in
+ * the line below to match the UICulture setting in the project file.
+ * [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+ */
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
-                                     // (used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly)] // where the generic resource dictionary is located
-                                                // (used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
+// ResourceDictionaryLocation.None
+// where theme specific resource dictionaries are located
+// (used if a resource is not found in the page,
+// or application resource dictionaries)
+// ResourceDictionaryLocation.SourceAssembly
+// where the generic resource dictionary is located
+// (used if a resource is not found in the page,
+// app, or any theme specific resource dictionaries)
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -44,7 +46,7 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]

382802eb8845480c02affb089a321da1e64fea83
tristhaus <tristhaus@yahoo.de>
2018-09-30 19:26:44 +0200

    Add first iteration of a coordinate system

--- PollyFoundation/MainWindow.xaml.cs
+++ PollyFoundation/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -14,13 +12,8 @@ namespace PollyFoundation
     public sealed partial class MainWindow : Window
     {
         private const double CanvasMargin = 10;
-        private double xmax;
-        private double xmin;
-        private double ymin;
-        private double ymax;
-        private double squareLength;
-        private double scale;
 
+        private CoordinateHelper coordinateHelper;
         private PollyController controller;
 
         public MainWindow()
@@ -34,62 +27,26 @@ namespace PollyFoundation
             this.controller.UpdateExpression(this.textBox0.Text);
 
             CoordinateSystemInfo csi = this.controller.CoordinateSystemInfo;
+            this.coordinateHelper = new CoordinateHelper(this.canvas.ActualWidth, this.canvas.ActualHeight, CanvasMargin, 20);
 
-            this.RefreshCanvasInfo(csi);
+            this.canvas.Children.Clear();
 
-            this.DrawCoordinateSystem(csi);
-
-            /*
-            // Make some data sets.
-            Brush[] brushes = { Brushes.Red, Brushes.Green, Brushes.Blue };
-            Random rand = new Random();
-            for (int data_set = 0; data_set < 3; data_set++)
-            {
-                int last_y = rand.Next((int)this.ymin, (int)this.ymax);
-
-                PointCollection points = new PointCollection();
-                for (double x = this.xmin; x <= this.xmax; x += step)
-                {
-                    last_y = rand.Next(last_y - 10, last_y + 10);
-                    if (last_y < this.ymin) last_y = (int)this.ymin;
-                    if (last_y > this.ymax) last_y = (int)this.ymax;
-                    points.Add(new Point(x, last_y));
-                }
-
-                Polyline polyline = new Polyline();
-                polyline.StrokeThickness = 1;
-                polyline.Stroke = brushes[data_set];
-                polyline.Points = points;
-
-                this.canvas.Children.Add(polyline);
-            }
-            */
+            this.DrawCoordinateSystem();
         }
 
-        private void RefreshCanvasInfo(CoordinateSystemInfo csi)
-        {
-            this.squareLength = Math.Min(this.canvas.Width, this.canvas.Height) - 2 * CanvasMargin;
-            this.scale = this.squareLength / (csi.EndX - csi.StartX);
-
-            this.xmin = 0.5 * (this.canvas.Width - this.squareLength) + CanvasMargin;
-            this.xmax = this.xmin + this.squareLength;
-            this.ymin = 0.5 * (this.canvas.Height - this.squareLength) + CanvasMargin;
-            this.ymax = this.ymin + this.squareLength;
-        }
-
-        private void DrawCoordinateSystem(CoordinateSystemInfo csi)
+        private void DrawCoordinateSystem()
         {
             // Make the X axis.
             GeometryGroup xaxis_geom = new GeometryGroup();
-            xaxis_geom.Children.Add(new LineGeometry(
-                new Point(this.xmin, 0.5 * (this.ymin + this.ymax)), new Point(this.xmax, 0.5 * (this.ymin + this.ymax))));
-            for (double x = csi.StartX + csi.TickInterval;
-                x <= csi.EndX; x += csi.TickInterval)
+            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0), this.coordinateHelper.Convert(-10.5, 0)));
+            for (int i = 1; i <= 10; i++)
             {
-                xaxis_geom.Children.Add(new LineGeometry(
-                    new Point(x * this.scale + this.xmin, 0.5 * (this.ymin + this.ymax) - CanvasMargin / 2),
-                    new Point(x * this.scale + this.xmin, 0.5 * (this.ymin + this.ymax) + CanvasMargin / 2)));
+                xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(+i, -0.3), this.coordinateHelper.Convert(+i, +0.3)));
+                xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-i, -0.3), this.coordinateHelper.Convert(-i, +0.3)));
             }
+
+            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, -0.3)));
+            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, +0.3)));
 
             Path xaxis_path = new Path();
             xaxis_path.StrokeThickness = 1;
@@ -98,28 +55,17 @@ namespace PollyFoundation
 
             this.canvas.Children.Add(xaxis_path);
 
-            // GeometryGroup myGroup = new GeometryGroup();
-            // myGroup.Children.Add(new LineGeometry(new Point(0, 0), new Point(100, 200)));
-            // Path myPath = new Path()
-            // {
-            //     Stroke = Brushes.Aquamarine,
-            //     StrokeThickness = 2,
-            //     Data = myGroup
-            // };
-            //
-            // this.canvas.Children.Add(myPath);
-
-            /*
-            // Make the Y ayis.
+            // Make the Y axis.
             GeometryGroup yaxis_geom = new GeometryGroup();
-            yaxis_geom.Children.Add(new LineGeometry(
-                new Point(xmin, 0), new Point(xmin, canvas.Height)));
-            for (double y = step; y <= canvas.Height - step; y += step)
+            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0, 10.5), this.coordinateHelper.Convert(0, -10.5)));
+            for (int i = 1; i <= 10; i++)
             {
-                yaxis_geom.Children.Add(new LineGeometry(
-                    new Point(xmin - margin / 2, y),
-                    new Point(xmin + margin / 2, y)));
+                yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, +i), this.coordinateHelper.Convert(+0.3, +i)));
+                yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, -i), this.coordinateHelper.Convert(+0.3, -i)));
             }
+
+            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(-0.3, 10.2)));
+            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(+0.3, 10.2)));
 
             Path yaxis_path = new Path();
             yaxis_path.StrokeThickness = 1;
@@ -127,7 +73,6 @@ namespace PollyFoundation
             yaxis_path.Data = yaxis_geom;
 
             this.canvas.Children.Add(yaxis_path);
-            //*/
         }
     }
 }

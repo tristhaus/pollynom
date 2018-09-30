40b026f853db00613a6b418b335299501351c362
tristhaus <tristhaus@yahoo.de>
2018-09-30 21:06:26 +0200

    Add drawing of graphs and some refactoring

--- PollyFoundation/MainWindow.xaml.cs
+++ PollyFoundation/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ namespace PollyFoundation
     {
         private const double CanvasMargin = 10;
 
+        private readonly Color[] graphColors = { Colors.Black, Colors.Blue, Colors.Green, Colors.Pink, Colors.Brown };
+
         private CoordinateHelper coordinateHelper;
         private PollyController controller;
 
@@ -27,52 +29,76 @@ namespace PollyFoundation
             this.controller.UpdateExpression(this.textBox0.Text);
 
             CoordinateSystemInfo csi = this.controller.CoordinateSystemInfo;
-            this.coordinateHelper = new CoordinateHelper(this.canvas.ActualWidth, this.canvas.ActualHeight, CanvasMargin, 20);
+            this.coordinateHelper = new CoordinateHelper(this.canvas.ActualWidth, this.canvas.ActualHeight, CanvasMargin, 21.2);
 
             this.canvas.Children.Clear();
 
             this.DrawCoordinateSystem();
+
+            this.DrawGraphs();
         }
 
         private void DrawCoordinateSystem()
         {
             // Make the X axis.
-            GeometryGroup xaxis_geom = new GeometryGroup();
-            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0), this.coordinateHelper.Convert(-10.5, 0)));
+            GeometryGroup xAxisGeometryGroup = new GeometryGroup();
+            xAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0), this.coordinateHelper.Convert(-10.5, 0)));
             for (int i = 1; i <= 10; i++)
             {
-                xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(+i, -0.3), this.coordinateHelper.Convert(+i, +0.3)));
-                xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-i, -0.3), this.coordinateHelper.Convert(-i, +0.3)));
+                xAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(+i, -0.3), this.coordinateHelper.Convert(+i, +0.3)));
+                xAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-i, -0.3), this.coordinateHelper.Convert(-i, +0.3)));
             }
 
-            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, -0.3)));
-            xaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, +0.3)));
+            xAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, -0.3)));
+            xAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(10.5, 0.0), this.coordinateHelper.Convert(10.2, +0.3)));
 
-            Path xaxis_path = new Path();
-            xaxis_path.StrokeThickness = 1;
-            xaxis_path.Stroke = Brushes.Black;
-            xaxis_path.Data = xaxis_geom;
+            Path xAxisPath = new Path();
+            xAxisPath.StrokeThickness = 1;
+            xAxisPath.Stroke = Brushes.Black;
+            xAxisPath.Data = xAxisGeometryGroup;
 
-            this.canvas.Children.Add(xaxis_path);
+            this.canvas.Children.Add(xAxisPath);
 
             // Make the Y axis.
-            GeometryGroup yaxis_geom = new GeometryGroup();
-            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0, 10.5), this.coordinateHelper.Convert(0, -10.5)));
+            GeometryGroup yAxisGeometryGroup = new GeometryGroup();
+            yAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0, 10.5), this.coordinateHelper.Convert(0, -10.5)));
             for (int i = 1; i <= 10; i++)
             {
-                yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, +i), this.coordinateHelper.Convert(+0.3, +i)));
-                yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, -i), this.coordinateHelper.Convert(+0.3, -i)));
+                yAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, +i), this.coordinateHelper.Convert(+0.3, +i)));
+                yAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(-0.3, -i), this.coordinateHelper.Convert(+0.3, -i)));
             }
 
-            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(-0.3, 10.2)));
-            yaxis_geom.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(+0.3, 10.2)));
+            yAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(-0.3, 10.2)));
+            yAxisGeometryGroup.Children.Add(new LineGeometry(this.coordinateHelper.Convert(0.0, 10.5), this.coordinateHelper.Convert(+0.3, 10.2)));
 
-            Path yaxis_path = new Path();
-            yaxis_path.StrokeThickness = 1;
-            yaxis_path.Stroke = Brushes.Black;
-            yaxis_path.Data = yaxis_geom;
+            Path yAxisPath = new Path();
+            yAxisPath.StrokeThickness = 1.5;
+            yAxisPath.Stroke = Brushes.Black;
+            yAxisPath.Data = yAxisGeometryGroup;
 
-            this.canvas.Children.Add(yaxis_path);
+            this.canvas.Children.Add(yAxisPath);
+        }
+
+        private void DrawGraphs()
+        {
+            for (int i = 0; i < this.controller.ExpressionCount; i++)
+            {
+                Brush brush = new SolidColorBrush(this.graphColors[i]);
+
+                var lists = this.controller.GetListsOfLogicalPointsByIndex(i);
+                foreach (var list in lists)
+                {
+                    Polyline polyLine = new Polyline();
+                    foreach (var point in list.Points)
+                    {
+                        polyLine.Points.Add(this.coordinateHelper.Convert(point.X, point.Y));
+                    }
+
+                    polyLine.Stroke = brush;
+                    polyLine.StrokeThickness = 1;
+                    this.canvas.Children.Add(polyLine);
+                }
+            }
         }
     }
 }
